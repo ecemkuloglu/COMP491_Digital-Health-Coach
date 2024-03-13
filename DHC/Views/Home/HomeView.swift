cbd684fcd9f5b132f3f17a5e872aea89d9694d77
KorhanDerin <korhanderin@gmail.com>
2024-03-13 23:56:45 +0300

    Placeholder HomeView added

--- DHC/Views/Home/HomeView.swift
+++ DHC/Views/Home/HomeView.swift
@@ -8,11 +8,62 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State private var selection = 3
+    @State private var showSignInView: Bool = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack {
+            bottomView
+        }
+        .onAppear {
+            let authUser = try? AuthenticationManager.shared.getAuthenticatedUser()
+            self.showSignInView = authUser == nil
+        }
+//        .fullScreenCover(isPresented: $showSignInView) {
+//            NavigationView {
+//                AuthenticationView(showSignInView: $showSignInView)
+//            }
+//        }
+    }   
+    
+    private var bottomView: some View {
+        TabView(selection: $selection) {
+            Text("Placeholder View")
+                .tabItem {
+                    Image(systemName: "shuffle")
+                }
+                .tag(1)
+            //SearchView()
+            Text("Search View")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                }
+                .tag(2)
+            //CategoryView()
+            Text("Category View")
+                .tabItem {
+                    Image(systemName: "house.fill")
+                }
+                .tag(3)
+            Text("Placeholder View")
+                .tabItem {
+                    Image(systemName: "star")
+                }
+                .tag(4)
+            //ProfileView(showSignInView: $showSignInView)
+            Text("Profile View")
+                .tabItem {
+                    Image(systemName: "person.circle.fill")
+                }
+                .tag(5)
+        }
+        .accentColor(.blue)
+        .navigationTitle("Digital Health Coah")
+        
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
