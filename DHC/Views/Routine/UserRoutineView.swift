44b183847554411d4f4d08f0a69540e5a2bbf831
ecemkuloglu <ekuloglu19@ku.edu.tr>
2024-03-25 22:39:54 +0300

    Routine view implemented

---
+++ DHC/Views/Routine/UserRoutineView.swift
@@ -0,0 +1,56 @@
+//
+//  UserRoutineView.swift
+//  DHC
+//
+//  Created by Lab on 21.03.2024.
+//
+
+import SwiftUI
+import HealthKit
+
+struct UserRoutineView: View {
+    @Binding var showUserRoutineView: Bool
+    @State private var stepCount: Int = 0
+    @State private var showExerciseView: Bool = false
+    
+    var body: some View {
+        VStack{
+            Text("Welcome to your routine page!")
+                .position(x: 130, y:40)
+                .padding()
+            //taken from developer.apple.com
+            CalendarView()
+                .frame(width: 320, height: 300)
+                .position(x: 180, y:-80)
+            Text("Step Count: \(stepCount)")
+            
+            Button("Look your exercises") {
+                showExerciseView.toggle()
+                        }
+                        .padding()
+                        .sheet(isPresented: $showExerciseView) {
+                            ExerciseView(showExerciseView: $showExerciseView)
+                        }
+            
+        }.padding()
+    }
+}
+
+struct CalendarView: View {
+    @State private var date = Date()
+    
+    var body: some View {
+        DatePicker(
+            "Start Date",
+            selection: $date,
+            displayedComponents: [.date]
+        )
+        .datePickerStyle(.graphical)
+    }
+}
+
+struct UserRoutineView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserRoutineView(showUserRoutineView: .constant(true))
+    }
+}
