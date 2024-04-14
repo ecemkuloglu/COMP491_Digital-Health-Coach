455ce182afcfc5c82c82ae10976cf4ae4d670bfe
KorhanDerin <korhanderin@gmail.com>
2024-04-14 19:52:02 +0300

    Added ExerciseManager functionality to retrieve exercise data from Firestore Database

---
+++ DHC/Models/ExerciseModel.swift
@@ -0,0 +1,14 @@
+//
+//  ExerciseModel.swift
+//  DHC
+//
+//  Created by Trio on 14.04.2024.
+//
+
+import Foundation
+
+struct ExerciseModel: Codable {
+    let name: String
+    let desc: String
+    let photoUrl: String
+}
