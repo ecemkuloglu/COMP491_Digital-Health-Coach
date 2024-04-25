9de2a8c09082cd6d6192581969f317fc3de1034e
ecemkuloglu <ekuloglu19@ku.edu.tr>
2024-04-25 18:05:36 +0300

    preferences changed

--- DHC/Views/Profile/PreferenceViewModel.swift
+++ DHC/Views/Profile/PreferenceViewModel.swift
@@ -12,50 +12,35 @@ class PreferenceViewModel: ObservableObject {
     @Published var userPreference: String?
 
     func fetchUserPreference(preferenceTitle: String) {
-        guard let currentUser = Auth.auth().currentUser else {
+        guard let userId = Auth.auth().currentUser?.uid else {
             print("User not authenticated.")
             return
         }
 
-        let db = Firestore.firestore()
-        let userRef = db.collection("users").document(currentUser.uid)
-        let field = "\(preferenceTitle.lowercased().replacingOccurrences(of: " ", with: "_"))_preference"
-
-        userRef.getDocument { documentSnapshot, error in
+        PreferenceManager.shared.fetchUserPreference(userId: userId, preferenceTitle: preferenceTitle) { preference, error in
             if let error = error {
                 print("Error fetching preference: \(error.localizedDescription)")
                 return
             }
-
-            if let document = documentSnapshot, document.exists {
-                let data = document.data()
-                DispatchQueue.main.async {
-                    self.userPreference = data?[field] as? String
-                }
-            } else {
-                print("Document does not exist.")
+            DispatchQueue.main.async {
+                self.userPreference = preference
             }
         }
     }
 
     func savePreference(preferenceTitle: String, option: String) {
-        guard let currentUser = Auth.auth().currentUser else {
+        guard let userId = Auth.auth().currentUser?.uid else {
             print("User not authenticated.")
             return
         }
 
-        let db = Firestore.firestore()
-        let userRef = db.collection("users").document(currentUser.uid)
-        let field = "\(preferenceTitle.lowercased().replacingOccurrences(of: " ", with: "_"))_preference"
-
-        userRef.setData([field: option], merge: true) { error in
+        PreferenceManager.shared.savePreference(userId: userId, preferenceTitle: preferenceTitle, option: option) { success, error in
             if let error = error {
                 print("Error saving preference for \(preferenceTitle): \(error.localizedDescription)")
-            } else {
-                print("Preference saved successfully for \(preferenceTitle): \(option)")
-                DispatchQueue.main.async {
-                    self.userPreference = option
-                }
+                return
+            }
+            DispatchQueue.main.async {
+                self.userPreference = option
             }
         }
     }
